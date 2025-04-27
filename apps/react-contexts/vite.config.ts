3acd867f0bc5659e69dfa1042498b4b19032af71
spaceofin <80182521+spaceofin@users.noreply.github.com>
2025-04-27 21:59:12 +0900

    [react-contexts]chore: set up project with shared configuration from monorepo root and clean up code

--- apps/react-contexts/vite.config.ts
+++ apps/react-contexts/vite.config.ts
@@ -1,7 +1,11 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig, mergeConfig } from "vite";
+import react from "@vitejs/plugin-react";
+import { baseConfig } from "../../vite.config.base";
 
 // https://vite.dev/config/
-export default defineConfig({
-  plugins: [react()],
-})
+export default mergeConfig(
+  baseConfig,
+  defineConfig({
+    plugins: [react()],
+  })
+);
