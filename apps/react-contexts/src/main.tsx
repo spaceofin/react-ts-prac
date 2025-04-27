13b36fb143df5eb1b1fa15e61694a3a426faf3d8
spaceofin <80182521+spaceofin@users.noreply.github.com>
2025-04-27 23:23:20 +0900

    [react-contexts]feat: add Header to App and create /color-context page

--- apps/react-contexts/src/main.tsx
+++ apps/react-contexts/src/main.tsx
@@ -1,10 +1,19 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.tsx'
+// import { StrictMode } from 'react'
+import { createRoot } from "react-dom/client";
+import "./index.css";
+import App from "./App.tsx";
+import Header from "./components/Header.tsx";
+import { BrowserRouter, Route, Routes } from "react-router";
+import ColorContextExample from "./pages/ColorContextExample.tsx";
 
-createRoot(document.getElementById('root')!).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
-)
+createRoot(document.getElementById("root")!).render(
+  // <StrictMode>
+  <BrowserRouter>
+    <Header />
+    <Routes>
+      <Route path="/" element={<App />} />
+      <Route path="/color-context" element={<ColorContextExample />} />
+    </Routes>
+  </BrowserRouter>
+  // </StrictMode>,
+);
---
+++ apps/react-contexts/src/pages/ColorContextExample.tsx
@@ -0,0 +1,3 @@
+export default function ColorContextExample() {
+  return <div className="p-10">Color Context Example</div>;
+}
